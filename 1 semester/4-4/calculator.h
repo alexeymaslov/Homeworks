d7e3df84cbb58781d486d19ad2beb71520864156
Alexey Maslov <mslv.alexey@yandex.ru>
2015-05-14 20:03:56 +0300

    Revert "Названия папок изменены"
    This reverts commit db79077f5e797b5e83d398301ecadad9e1f57db6.

--- 1 semester/4-4/calculator.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int getPriority(char symbol);
-void changeToPostfix(char *firstLine, char *secondLine);
-double getResult(char symbol, double digitOne, double digitTwo);
-double calculate(char *line, const int size);
