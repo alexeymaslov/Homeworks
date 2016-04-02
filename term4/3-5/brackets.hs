a6f23648d06c2a1ac0eb4952c2d42e590604c2b6
Alexey Maslov <mslv.alexey@yandex.ru>
2016-04-02 16:32:21 +0300

    Теперь функция принимает строку с любыми символами и обрабатывает скобки вида {} ()

--- term4/3-5/brackets.hs
+++ term4/3-5/brackets.hs
@@ -1,8 +1,16 @@
+import Data.Maybe
+
 checkBrackets :: String -> Bool
-checkBrackets s = (checkBrackets_helper s 0) == 0
+checkBrackets s = checkBrackets_helper s ""
     where
-    checkBrackets_helper :: String -> Int -> Int
-    checkBrackets_helper _ (-1) = -1                                  --Случай, когда закрывающих скобок больше чем открывающих
-    checkBrackets_helper [] i = i
-    checkBrackets_helper (']':xs) i = checkBrackets_helper xs (i - 1)
-    checkBrackets_helper ('[':xs) i = checkBrackets_helper xs (i + 1)
+    checkBrackets_helper :: String -> String -> Bool
+    checkBrackets_helper [] stack = null stack
+    checkBrackets_helper (x:xs) stack 
+        | not (elem x "[](){}")             = checkBrackets_helper xs stack
+        | elem x "[({"                      = checkBrackets_helper xs ((getSameClosing x):stack)      --Добавляем соответствующую закрывающую скобку в стек
+        | elem x "])}" && not (null stack)  = x == head stack && checkBrackets_helper xs (tail stack) --Проверяем, что скобка в стеке соответствует, и удаляем ее
+        | otherwise                         = False
+        where
+        getSameClosing :: Char -> Char
+        getSameClosing x = fromJust $ lookup x (zip "[{(" "]})")
+    
