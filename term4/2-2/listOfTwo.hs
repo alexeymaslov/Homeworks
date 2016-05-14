08b9e827145760b389dff2ddd81d35340d9f58c6
Alexey Maslov <mslv.alexey@yandex.ru>
2016-05-14 21:12:33 +0300

    Теперь без рекурсии

--- term4/2-2/listOfTwo.hs
+++ term4/2-2/listOfTwo.hs
@@ -1,5 +1,2 @@
-createList :: Integer -> [Integer]
-createList n = creation_helper n [] where
-    creation_helper :: Integer -> [Integer] -> [Integer]
-    creation_helper 0 a = a
-    creation_helper n a = creation_helper (n - 1) ((2 ^ n) : a) 
+createList :: Int -> [Int]
+createList n = map ((^) 2 ) (take n [1, 2..])
